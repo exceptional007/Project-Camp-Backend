19c0ff72ce0701696750ea156c3007cefb1cc7cc
exceptional007 <akshhatsri7843@gmail.com>
2025-10-02 23:59:37 +0530

    Added DotEnv Dependency

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env",
+});
+
+let myusername = process.env.db_username
+
+console.log("Value: ", myusername);
+
+
 console.log("Start of Backend Project");
